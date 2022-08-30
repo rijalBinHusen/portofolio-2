3b825a46d0641e1bf3fa4ffaf780dd23aeed8d30
rijal <jeryzal333@gmail.com>
2022-08-30 20:48:44 +0700

    liststing blogs, displaying blogs

--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,15 @@
         <p>Take a look at <NuxtLink to="/blog">my blog</NuxtLink></p>
       </header>
     </section>
+      <ul>
+        <li v-for="post in posts" :key="post._path">
+          <nuxt-link :to="`${post._path}`"> {{ post.title }} </nuxt-link>
+        </li>
+      </ul>
+        <!-- <pre>{{ posts }}</pre> -->
   </main>
 </template>
+
+<script setup>
+    const { data: posts } = await useAsyncData('navigation', () => fetchContentNavigation())
+</script>
