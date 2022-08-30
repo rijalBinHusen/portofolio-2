aa324760ddabce81882181050a4c3003c8e87215
rijal <jeryzal333@gmail.com>
2022-08-30 20:17:33 +0700

    seperate main and blog pages, render test.md

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <section class="hero-section">
+      <header>
+        <h1 class="font-black text-8xl">Welcome to my site</h1>
+        <p>Take a look at <NuxtLink to="/blog">my blog</NuxtLink></p>
+      </header>
+    </section>
+  </main>
+</template>
