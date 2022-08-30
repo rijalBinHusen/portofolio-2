cdd1134939ee5a4aed00930a92fc0cfcf92dd94d
rijal <jeryzal333@gmail.com>
2022-08-31 05:21:37 +0700

    display layouts navbar

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>  
+    <div>    
+        <nav>      
+            <NuxtLink to="/" class="link">Home</NuxtLink>      
+            <NuxtLink to="/blog" class="link">Blog</NuxtLink>    
+        </nav>    
+        <main>      
+            <slot />    
+        </main>  
+    </div>
+</template>
+
+<style>
+    .link {  margin-right: 1rem;}
+</style>
--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,12 +1,7 @@
 <template>
   <main>
-    <section class="hero-section">
-      <header>
-        <h1 class="font-black text-8xl">Welcome to my site</h1>
-        <p>Take a look at <NuxtLink to="/blogs">my blog</NuxtLink></p>
-      </header>
-    </section>
-      <ul>
+    <h1>My blogs</h1>
+    <ul>
         <li v-for="post in posts" :key="post._path">
           <nuxt-link :to="`${post._path}`"> {{ post.title }} </nuxt-link>
         </li>
