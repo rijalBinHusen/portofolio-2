acb750d0075e8131c80f66ad18017e799c2ed192
rijal <jeryzal333@gmail.com>
2022-09-01 05:05:45 +0700

    fix transition problem

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,9 +1,11 @@
 <template>
-  <ContentDoc
-    :path="$route.params.slug ? `/blog/${$route.params.slug[0]}` : '/blog'"
-  >
-    <template #not-found>
-      <h2>Blog slug ({{ $route.params.slug }}) not found</h2>
-    </template>
-  </ContentDoc>
+  <div>
+    <ContentDoc
+      :path="$route.params.slug ? `/blog/${$route.params.slug[0]}` : '/blog'"
+    >
+      <template #not-found>
+        <h2>Blog slug ({{ $route.params.slug }}) not found</h2>
+      </template>
+    </ContentDoc>
+  </div>
 </template>
