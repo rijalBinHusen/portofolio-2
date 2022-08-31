9254b7d3396ef2e377015e7f1ca86eff07258363
rijal <jeryzal333@gmail.com>
2022-08-31 21:30:00 +0700

    problem with transition

---
+++ components/content/BlogPosts.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Blog</h1>
+  <ul>
+    <li v-for="{ _path: slug, title } in blogPosts" :key="slug">
+      <NuxtLink :to="slug">{{ title }}</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const blogPosts = await queryContent('/blog')
+  .sort({ date: -1 }) // show latest articles first
+  .where({ _partial: false }) // exclude the Partial files
+  .find();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="hero-section">
       <header>
         <h1 class="font-black text-8xl">Welcome to my site</h1>
-        <p>Take a look at <NuxtLink to="/blogs">my blog</NuxtLink></p>
+        <p>Take a look at <NuxtLink to="/blog">my blog</NuxtLink></p>
       </header>
     </section>
   </main>
