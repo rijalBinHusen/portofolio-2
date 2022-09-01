122c58f01135bf0805a7b09e6d38597b88620f73
rijal <jeryzal333@gmail.com>
2022-09-02 03:36:12 +0700

    add class to activate typography in blog

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,6 +2,7 @@
   <div>
     <ContentDoc
       :path="$route.params.slug ? `/blog/${$route.params.slug[0]}` : '/blog'"
+      class="prose p-16 max-w-3xl m-auto"
     >
       <template #not-found>
         <h2>Blog slug ({{ $route.params.slug }}) not found</h2>
