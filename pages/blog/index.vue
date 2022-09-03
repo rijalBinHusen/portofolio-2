3f831e822f356fd9692e2243ace71d841941c5da
rijal <jeryzal333@gmail.com>
2022-09-03 16:53:59 +0700

    everything is normal here

---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h1>Blog</h1>
+  <ul>
+    <li v-for="{ _path: slug, title } in blogPosts" :key="slug">
+      <NuxtLink :to="slug">{{ title }}</NuxtLink>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script setup lang="ts">
+const blogPosts = await queryContent('/blog')
+  .sort({ date: -1 }) // show latest articles first
+  .where({ _partial: false }) // exclude the Partial files
+  .find();
+</script>
