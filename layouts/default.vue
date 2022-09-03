00daef7a919fa380232cb41b6f918db47ea0ae49
rijal <jeryzal333@gmail.com>
2022-09-04 06:24:03 +0700

    add some icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
 <template>  
     <div>    
         <nav>      
-            <NuxtLink to="/" class="link">Home</NuxtLink>      
-            <NuxtLink to="/blog" class="link">Blog</NuxtLink>    
+            <NuxtLink to="/" class="link">
+                Home
+                <font-awesome-icon icon="fa-solid fa-home" class="text-sm" />
+                <!-- <font-awesome-icon :icon="['fas', 'home']"  class="text-sm" /> -->
+            </NuxtLink>      
+            <NuxtLink to="/blog" class="link">
+                Blog
+                <font-awesome-icon icon="fa-solid fa-newspaper" class="text-sm" />
+            </NuxtLink>    
         </nav>    
         <main>      
             <slot />    
