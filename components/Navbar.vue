af59d222847f8acb3ef77db3708f702dd01bdc7b
rijal <jeryzal333@gmail.com>
2022-09-04 20:44:13 +0700

    navbar look good

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,19 +2,19 @@
     <!-- component -->
 <nav id="header" class="w-full z-30 top-10 shadow-lg border-b border-gray-300 py-1 rounded-lg">
       <div class="w-full flex items-center justify-between mt-0 px-6 py-2">
-         <label for="menu-toggle" class="cursor-pointer md:hidden block">
-            <svg class="fill-current text-blue-600" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+         <label for="menu-toggle" v-if="!showMenu" @click="showMenu = !showMenu" class="cursor-pointer md:hidden block">
+            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
                <title>menu</title>
                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
             </svg>
          </label>
          <input class="hidden" type="checkbox" id="menu-toggle">
          
-         <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
+         <div v-if="showMenu" class="md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
             <!-- Navigation link -->
             <nav>
                <ul class="md:flex items-center justify-between text-base text-black pt-4 md:pt-0">
-                  <li v-for="nav in navs" :key="nav.title">
+                  <li @click="showPage" v-for="nav in navs" :key="nav.title">
                      <NuxtLink 
                         :to="nav.route" 
                         class="inline-block no-underline hover:text-blue-600 font-medium text-lg py-2 px-4 lg:-ml-2"
@@ -29,10 +29,12 @@
             <!-- Navigation link -->
          </div>
          
-         <div class="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4" id="nav-content">
+         <div v-if="!showMenu" class="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4" id="nav-content">
             <div class="auth flex items-center w-full md:w-full">
-               <!-- <button class="bg-transparent text-gray-800  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700">Sign in</button>
-               <button class="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100">Sign up</button> -->
+               <a href="https://github.com/rijalBinHusen">
+                  <font-awesome-icon class="text-xl" :icon="['fab', 'github']" />
+               </a>
+               <!-- <button class="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100">Sign up</button> -->
             </div>
          </div>
       </div>
@@ -47,10 +49,34 @@
       { title: 'Github repo', route: '/projects', icon: 'diagram-project'}
    ]
 
+   const showMenu = ref(false)
+
    const route = useRoute()
 
    const isActive = (nowRoute: String) => {
       // console.log(route)
       return nowRoute.includes(route.name)
    }
+
+   const mobileView = ref(false)
+
+   const showPage = () => {
+      if(mobileView.value) {
+         showMenu.value = false
+      }
+   }
+
+   const checkScreen = () => {
+        if(window.innerWidth <= 750) {
+            mobileView.value = true;
+            showMenu.value = false
+            return
+        }
+            mobileView.value = false;
+            showMenu.value = true
+      }
+
+   onBeforeMount(() => {
+      window.addEventListener('resize', checkScreen)
+   })
 </script>
