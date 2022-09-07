a9ecabd8656bd53f3405df110ba2f1f14d89d464
rijal <jeryzal333@gmail.com>
2022-09-07 18:27:36 +0700

    blog card look good

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,18 +1,27 @@
 <template>
-<div>
-  <h1>Blog</h1>
-  <div>
-    <div v-for="{ _path: slug, title, date } in blogPosts" :key="slug">
+  <div class="flex flex-wrap flex-row overflow-auto">
+    <div 
+      class="mt-5 cursor-pointer flex-shrink px-4 w-screen md:w-1/3 lg:px-6 wow fadeInUp" 
+      v-for="{ _path: slug, title, date, img, description } in blogPosts" :key="slug"
+    >
       <NuxtLink class="no-underline" :to="slug">
-          <div class="text-xl font-extrabold">
-            {{ title }}
+        <div class="justify-center">
+          <div class="py-2 rounded-lg w-96 text-gray-900 bg-white">
+            <img :src="img" alt="blog" class="justify-center mx-4 w-11/12 rounded-lg">
+            <div class="px-6 py-2 w-full rounded-t-lg">
+                <p class="text-xl font-extrabold mb-2">
+                  {{ title }}
+                </p>
+                <p class="border-b pb-2">
+                  {{ description }}
+                </p>
+              <div>
+                <font-awesome-icon icon="calendar-days"/> {{ date.slice(0, 10) }}
+              </div>
+            </div>
           </div>
-          <span>
-            <font-awesome-icon icon="calendar-days"/> {{ date.slice(0, 10) }}
-          </span>
+        </div>
       </NuxtLink>
-
-    </div>
   </div>
 </div>
 </template>
