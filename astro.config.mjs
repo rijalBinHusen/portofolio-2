0518277bf7634bbde610cb8557c74db776e7dd12
rijal <jeryzal333@gmail.com>
2022-12-22 05:43:30 +0700

    add site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,20 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 // import preact from "@astrojs/preact";
-import vue from '@astrojs/vue'
+import vue from "@astrojs/vue";
 
 // tailwind
-import tailwind from '@astrojs/tailwind'
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind({
-    // provide custom path to a tailwind config
-    config: { path: './tailwind.config.cjs' }
-  })],
-  publicDir: './public'
+  site: "https://rijalbinhusen.netlify.app",
+  integrations: [
+    vue(),
+    tailwind({
+      // provide custom path to a tailwind config
+      config: { path: "./tailwind.config.cjs" },
+    }),
+  ],
+  publicDir: "./public",
 });
