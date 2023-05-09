eef321e5721c59ea37dda8fdb5bbecc2f180d7c7
rijalBinHusen <jeryzal333@gmail.com>
2023-05-09 16:21:20 +0700

    show projects on landing page

--- src/pages/projects/projects.vue
+++ src/pages/projects/projects.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="overflow-auto">
-  <div class="flex font-semibold text-3xl justify-center md:mt-28 my-8">
-    <h1 class="heading">ALL REPOSITORIES</h1>
+  <div :class="['flex font-semibold text-3xl justify-center', isOnlyProjects ? '' : ' md:mt-28 my-8']">
+    <h1 class="heading">{{ isOnlyProjects ? 'Recent projects' : 'ALL REPOSITORIES' }}</h1>
   </div>
 <!-- <pre> {{ repos }} </pre> -->
-    <div v-if="repos.length" class="flex flex-wrap flex-row mb-2">
+    <div v-if="repos.length" :class="['flex flex-wrap flex-row mb-2', isOnlyProjects ? 'justify-center' : '']">
         <!-- Card repos -->
         <CardProject 
             v-for="repo in repos" :key="repo.name"
@@ -20,16 +20,32 @@
 </template>
 
 <script setup lang='ts'>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, defineProps } from 'vue';
 import CardProject from "./CardProject.vue";
 import { Main } from '../../utils/githubResponse';
 import { getRepositories } from "../../scripts/getRepositories"
 
 let repos = ref(<Main[]>[])
 
+const props = defineProps({
+    isOnlyProjects: Boolean,
+})
+
 onMounted( async () => {
     if(!repos.value?.length) {
-        repos.value = await getRepositories()
+        const repositories = await getRepositories()
+        let count = 0;
+        console.log(props.isOnlyProjects)
+        if(props.isOnlyProjects) {
+            repos.value = repositories.filter((repo) => {
+                if(count < 2 && repo.html_url) {
+                    count++
+                    return repo
+                }
+            })
+        } else {
+            repos.value = repositories;
+        }
     }
 })
 
