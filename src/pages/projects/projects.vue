a0fb73a30f9f4b1f65d02c2839171a9741a1f35c
rijalBinHusen <jeryzal333@gmail.com>
2023-05-09 15:45:26 +0700

    I seperate function to get repositories

--- src/pages/projects/projects.vue
+++ src/pages/projects/projects.vue
@@ -23,51 +23,14 @@
 import { ref, onMounted } from 'vue';
 import CardProject from "./CardProject.vue";
 import { Main } from '../../utils/githubResponse';
+import { getRepositories } from "../../scripts/getRepositories"
 
 let repos = ref(<Main[]>[])
 
-const getRepository = async () => {
-    fetch('https://api.github.com/users/rijalBinHusen/repos')
-	.then(response => response.json())
-	.then(data => {
-        renewLists(data)
-        window.localStorage.setItem('repository', JSON.stringify({
-            repos: data,
-            expired: new Date().getTime() + 86400000
-        })
-        )
-    })
-	.catch(err => console.error(err));
-}
-
-const renewLists = (newRepos: Main[]) => {
-    repos.value = newRepos.sort(function (a, b) {
-          // return a?.pushed_at - b?.pushed_at;
-          let x = a.pushed_at;
-          let y = b.pushed_at;
-          if (x < y) {
-            return 1;
-          }
-          if (x > y) {
-            return -1;
-          }
-          return 0;
-        });
-}
-
-onMounted(() => {
+onMounted( async () => {
     if(!repos.value?.length) {
-        // get local storage firstt
-        let getLocalRepos: { repos: Main[], expired: number } = JSON.parse(window.localStorage.getItem('repository') || '{"repos":null,"expired":0}')
-        // if local storage expired
-        if(!getLocalRepos?.repos || new Date().getTime() > getLocalRepos.expired ) {
-            // we do anything for you
-            getRepository()
-            return
-        }
-        renewLists(getLocalRepos.repos)
+        repos.value = await getRepositories()
     }
-
 })
 
 </script>
---
+++ src/scripts/getRepositories.ts
@@ -0,0 +1,35 @@
+import { Main } from '../utils/githubResponse';
+
+export const getRepositories = async () => {
+    // get local storage firstt
+    let localRepositories: { repos: Main[], expired: number } = JSON.parse(window.localStorage.getItem('repository') || '{"repos":null,"expired":0}')
+    
+    // if local storage expired
+    if(!localRepositories?.repos || new Date().getTime() > localRepositories.expired ) {
+        fetch('https://api.github.com/users/rijalBinHusen/repos')
+        .then(response => response.json())
+        .then(data => {
+            let gotRepositories = {
+                repos: data,
+                expired: new Date().getTime() + 86400000
+            }
+            window.localStorage.setItem('repository', JSON.stringify(gotRepositories)
+            )
+            localRepositories = gotRepositories
+        })
+        .catch(err => console.error(err));
+    }
+    return localRepositories.repos.sort(function (a, b) {
+        // return a?.pushed_at - b?.pushed_at;
+        let x = a.pushed_at;
+        let y = b.pushed_at;
+        if (x < y) {
+          return 1;
+        }
+        if (x > y) {
+          return -1;
+        }
+        return 0;
+      });
+
+}
