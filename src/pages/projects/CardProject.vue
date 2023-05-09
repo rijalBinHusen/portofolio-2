17551f12283f4cd5f3395f7c792e627f61b9b783
rijalBinHusen <jeryzal333@gmail.com>
2023-05-09 11:29:24 +0700

    projects card component

---
+++ src/pages/projects/CardProject.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="w-screen md:w-1/3 md:px-6 wow fadeInUp" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;">
+        <!-- service block -->
+        <div class="rounded-lg py-6 px-9 mt-5 bg-red-100 bg-opacity-50 shadow-lg border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2 w-full">
+            <h3 class="text-lg leading-normal mb-3 font-semibold text-black">
+                {{ repoName }}
+            </h3>
+            <p class="mb-3">Description: <span class="text-gray-500 ">{{ description || "No description" }}</span></p>
+            <p class="text-sm"> Last update: 
+                <span class="text-gray-500">{{ new Date(pushedAt).toISOString().slice(0, 10) }} </span>
+            </p>
+            <p ><span class="text-sm">Tech: </span>
+                <!-- <pre> {{ repo?.topics }} </pre> -->
+                <Icon 
+                    v-for="lang in topics" 
+                    :key="lang" width="18" class="inline"
+                    :icon="brandIconColor(lang)"
+                    style="margin-right:2px"
+                />
+                
+            </p>
+            <div class="flex mt-5 boder border-gray-400 border-t pt-3 justify-between">
+                <a :href="htmlUrl" class="font-light" target="_blank">
+                    <Icon
+                        width="18" class="inline"
+                        icon="fa-brands:github"
+                    />
+                    Source
+                </a>
+
+                <a v-if="homepage" :href="homepage" class="font-light" target="_blank">
+                    <Icon
+                        width="18" class="inline"
+                        icon="mdi:web"
+                    />
+                    Demo
+                </a>
+                
+            </div>
+        </div>
+        <!-- end service block -->
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import { defineProps } from "vue"
+import { Icon } from "@iconify/vue"
+
+const props = defineProps({
+    repoName: String,
+    description: String,
+    pushedAt: String,
+    topics: Array,
+    homepage: String,
+    htmlUrl: String,
+})
+
+
+const brandIconColor = (lang?: String) => {
+
+if(lang === 'html') {
+    return 'uil:html5'
+}
+
+else if(lang === 'typescript') {
+    return 'logos:typescript-icon'
+}
+
+else if(lang === 'tailwind') {
+    return 'logos:tailwindcss-icon'
+}
+
+else if(lang === 'nodejs') {
+    return 'logos:nodejs-icon'
+}
+
+else if(lang === 'css') {
+    return 'vaadin:css'
+} 
+
+else if(lang === 'astrojs') {
+    return 'vscode-icons:file-type-astro'
+}
+
+else if(lang === 'mysql') {
+    return 'carbon:sql'
+}
+
+return 'logos:'+lang
+}
+
+</script>
--- src/pages/projects/projects.vue
+++ src/pages/projects/projects.vue
@@ -4,50 +4,25 @@
     <h1 class="heading">ALL REPOSITORIES</h1>
   </div>
 <!-- <pre> {{ repos }} </pre> -->
-  <div class="flex flex-wrap flex-row mb-2">
-    <!-- Card repos -->
-    <div v-for="repo in repos" :key="repo.name" class="w-screen md:w-1/3 md:px-6 wow fadeInUp" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;">
-        <!-- service block -->
-        <div class="rounded-lg py-6 px-9 mt-5 bg-red-100 bg-opacity-50 shadow-lg border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2 w-full">
-            <h3 class="text-lg leading-normal mb-3 font-semibold text-black">
-                {{ repo?.name }}
-                <!-- <font-awesome-icon :icon="['fab', 'github']" />  -->
-                <Icon  width="18" class="inline" icon="fa-brands:github" />
-            </h3>
-            <p class="mb-3">Description: <span class="text-gray-500 ">{{ repo?.description || "No description" }}</span></p>
-            <p class="text-sm"> Last update: 
-                <span class="text-gray-500">{{ new Date(repo?.pushed_at).toLocaleDateString() }} </span>
-            </p>
-            <p ><span class="text-sm">Tech: </span>
-                <!-- <pre> {{ repo?.topics }} </pre> -->
-                <Icon 
-                    v-for="lang in repo?.topics" 
-                    :key="lang" width="18" class="inline"
-                    :icon="brandIconColor(lang)"
-                    style="margin-right:2px"
-                />
-                
-            </p>
-        </div>
-        <!-- end service block -->
-      </div>
+    <div v-if="repos.length" class="flex flex-wrap flex-row mb-2">
+        <!-- Card repos -->
+        <CardProject 
+            v-for="repo in repos" :key="repo.name"
+            :repoName="repo.name" 
+            :description="repo.description" 
+            :pushedAt="repo.pushed_at" 
+            :topics="repo.topics" 
+            :homepage="repo.homepage" 
+            :htmlUrl="repo.html_url" 
+        />
     </div>
-    <!-- End of card repos -->
-    <!-- Skeleton loading -->
-    <!-- <div v-else v-for="num in [ ...Array(12).keys() ]" :key="num" class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;">
-        <div class="rounded-lg lg:w-80 py-8 px-6 mt-5 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
-            <Skeleton :rows="1" />
-        </div>
-
-    </div> -->
-    <!-- End of skeleton loading -->
 </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, onMounted } from 'vue';
+import CardProject from "./CardProject.vue";
 import { Main } from '../../utils/githubResponse';
-import { Icon } from "@iconify/vue"
 
 let repos = ref(<Main[]>[])
 
@@ -94,40 +69,5 @@ onMounted(() => {
     }
 
 })
-
-
-
-const brandIconColor = (lang: String) => {
-
-    if(lang === 'html') {
-        return 'uil:html5'
-    }
-
-    else if(lang === 'typescript') {
-        return 'logos:typescript-icon'
-    }
-
-    else if(lang === 'tailwind') {
-        return 'logos:tailwindcss-icon'
-    }
-
-    else if(lang === 'nodejs') {
-        return 'logos:nodejs-icon'
-    }
-
-    else if(lang === 'css') {
-        return 'vaadin:css'
-    } 
-    
-    else if(lang === 'astrojs') {
-        return 'vscode-icons:file-type-astro'
-    }
-
-    else if(lang === 'mysql') {
-        return 'carbon:sql'
-    }
-
-    return 'logos:'+lang
-}
 
 </script>
